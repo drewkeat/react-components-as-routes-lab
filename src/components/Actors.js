a8edd788f91baabb3d6b2441a39e295870f1b2b3
Drew Keat <thedrewkeat@gmail.com>
2021-11-28 23:38:41 -0700

    commit tree 4d262677418e6dc81c4925791546e8d32a770935

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,18 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actors.map((actor, index) => (
+        <div key={index}>
+          <h3>{actor.name}</h3>
+          <ul>
+            {actor.movies.map(movie => (
+              <li>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      {/* make a new <div> for each actor. The <div> should contain the actor's name and a <ul> with a list of their movies. */}
     </div>
   );
 };
